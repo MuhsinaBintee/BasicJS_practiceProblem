23e2db42a2d2465fa7cf7fcbf034f00bc08f7b81
Bintee <80423437+MuhsinaBintee@users.noreply.github.com>
2023-06-22 10:11:22 +0600

    Create index.js

---
+++ chapter_1PS/index.js
@@ -0,0 +1,62 @@
+//Q1
+//let age=prompt("enter your age :")
+/*if(age>10 && age<20){
+  console.log("age lies between 10 and 20")
+}else{
+   console.log("age doesn't lie between 10 and 20")
+}*/
+//Q2
+/*
+switch(age){
+  case "11":
+    console.log("age lies between 10 and 20")
+    break;
+  case "12":
+    console.log("age lies between 10 and  20")
+    break;
+  case "13":
+    console.log("age lies between 10 and  20")
+    break;
+  case "14":
+    console.log("age lies between 10 and  20")
+    break;
+  case "15":
+    console.log("age lies between 10 and  20")
+    break;
+  case "16":
+    console.log("age lies between 10 and  20")
+    break;
+  case "17":
+    console.log("age lies between 10 and 20")
+    break;
+  case "18":
+    console.log("age lies between 10 and  20")
+    break;
+  case "19":
+    console.log("age lies between 10 and 20")
+    break;
+  default:
+    console.log("age doesn't lie between 10 and  20")
+}
+*/
+//Q3
+/*
+let n=parseInt(prompt("enter a number"))
+if(n%2==0 && n%3==0){
+  console.log("n divisible by 2 and 3")
+}else{
+  console.log("n is not divisible by 2 and 3")
+}
+*/
+//Q4
+/*
+let n=parseInt(prompt("enter a number"))
+if(n%2==0 || n%3==0){
+  console.log("n divisible by 2 or 3")
+}else{
+  console.log("n is not divisible by 2 and 3")
+}
+*/
+//Q5
+let age=parseInt(prompt("Enter your age :"))
+age<18 ? console.log("You can not drive") : console.log("You can drive")
